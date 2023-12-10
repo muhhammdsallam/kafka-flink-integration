aeeb88ea0d03476f95cfe433460b2bec8182d5bc
msallam <m.sallam2622000@gmail.com>
2023-12-10 12:00:05 +0200

    updated processing code

--- src/main/java/org/conduktor/demos/FlinkKafkaConsumer.java
+++ src/main/java/org/conduktor/demos/FlinkKafkaConsumer.java
@@ -13,12 +13,12 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class FlinkKafkaConsumer {
 
-    public static String prettyPrintJsonUsingDefaultPrettyPrinter(String uglyJsonString) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        Object jsonObject = objectMapper.readValue(uglyJsonString, Object.class);
-        String prettyJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
-        return prettyJson;
-    }
+//    public static String prettyPrintJsonUsingDefaultPrettyPrinter(String uglyJsonString) throws JsonProcessingException {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        Object jsonObject = objectMapper.readValue(uglyJsonString, Object.class);
+//        String prettyJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
+//        return prettyJson;
+//    }
 
     public static void main(String[] args) throws Exception {
         String brokers = "localhost:9092";
@@ -35,36 +35,38 @@ public class FlinkKafkaConsumer {
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .build();
 
-
-        env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
-
         // Using WatermarkStrategy for event time
         DataStream<String> kafkaDataStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
 
         // Processing logic goes here
         // TODO: Add your processing logic here to process json messages from Kafka
-//        DataStream<String> processedDataStream = kafkaDataStream
-//                .map(jsonString -> {
-//                    // Parse the JSON string into a JsonNode
-//                    ObjectMapper objectMapper = new ObjectMapper();
-//                    JsonNode jsonNode = objectMapper.readTree(jsonString);
-//
-//                    // Perform your processing on the JsonNode
-//                    // For example, extract a field and modify it
-//                    String modifiedField = jsonNode.get("user").asText() + "_modified";
-//
-//                    // Create a new JsonNode with the modified field
-//                    JsonNode modifiedJsonNode = jsonNode.with(modifiedField);
-//
-//                    // Serialize the modified JsonNode back to a JSON string
-//                    return objectMapper.writeValueAsString(modifiedJsonNode);
-//                });
+        DataStream<WikimediaChangeSchema> deserialzedDataStream = kafkaDataStream.map(jsonString -> {
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode jsonNode = objectMapper.readTree(jsonString);
 
-        // printing the messages to the console
-//        String formattedDataStream = prettyPrintJsonUsingDefaultPrettyPrinter(kafkaDataStream.toString());
+                // For example, extract a field and modify it
+                long id = jsonNode.get("id").asLong();
+                String type = jsonNode.get("type").asText();
+                String user = jsonNode.get("user").asText();
+
+                // Creating a wikimedia change object
+                WikimediaChangeSchema wikimediaChangeSchema = new WikimediaChangeSchema(id, type, user);
+
+                // print the object
+//            System.out.println(wikimediaChangeSchema.toString());
+
+                return wikimediaChangeSchema;
+            }
+            catch (JsonProcessingException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        });
 
 //        kafkaDataStream.getType();
-        kafkaDataStream.print();
+        deserialzedDataStream.print();
 //        System.out.println(formattedDataStream);
 
         // Execute the Flink job
---
+++ src/main/java/org/conduktor/demos/WikimediaChangeSchema.java
@@ -0,0 +1,47 @@
+package org.conduktor.demos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class WikimediaChangeSchema {
+
+    @JsonProperty("id")
+    private long id;
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("user")
+    private String user;
+
+
+    WikimediaChangeSchema(long id, String type, String user) {
+        this.id = id;
+        this.type = type;
+        this.user = user;
+
+    }
+
+//    public static WikimediaChangeSchema fromString(String line) {
+//        String[] parts = line.split("\\|");
+//        return new WikimediaChangeSchema(Integer.parseInt(parts[0]), parts[1], parts[2], parts[3]);
+//    }
+
+    @Override
+    public String toString() {
+        return "WikimediaChangeSchema{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                ", user='" + user + '\'' +
+                '}';
+    }
+    public long getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+}
